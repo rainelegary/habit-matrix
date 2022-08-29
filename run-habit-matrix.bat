6005d4f6ce36234047b5daabd85fb5224c4fd245
RaineLegary <rainethcubed@gmail.com>
2022-08-28 21:08:56 -0600

    gitignore

--- run-habit-matrix.bat
+++ run-habit-matrix.bat
@@ -1,11 +1,12 @@
 @echo off
 set RUN_MODE="app"
-set WORKING_DIR="C:/Users/Raine/OneDrive/Desktop/math_and_coding/projects/habit-matrix/"
+::set YAML_DIR="C:/Users/Raine/OneDrive/Desktop/math_and_coding/project-related files/habit matrix/YAML/"
+set YAML_DIR="C:/Users/Raine/OneDrive/Desktop/math_and_coding/projects/habit-matrix/DataManagement/DataYAML/"
 
 TITLE habit matrix
 
 call conda activate base
-"C:\Users\Raine\anaconda3\python.exe" "C:\Users\Raine\OneDrive\Desktop\math_and_coding\projects\habit-matrix\main.py" %RUN_MODE% %WORKING_DIR%
+"C:\Users\Raine\anaconda3\python.exe" "C:\Users\Raine\OneDrive\Desktop\math_and_coding\projects\habit-matrix\main.py" %RUN_MODE% %YAML_DIR%
 call conda deactivate
 
 PAUSE
